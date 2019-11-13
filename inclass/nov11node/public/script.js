9cf85881826d350ab020ffa6b078767efc72ea18
dennisperea <54592924+dennisperea@users.noreply.github.com>
2019-11-13 14:25:49 -0500

    removed bad installation of node

--- inclass/nov11node/public/script.js
+++ inclass/nov11node/public/script.js
@@ -1,1 +1,53 @@
-console.log("loading script");
+async function showSongs(){
+    let songsJson = await fetch('api/songs');
+    let songs = await songsJson.json();
+    let songsDiv = document.getElementById("songs");
+    console.log(songs);
+
+    for (i in songs){
+        songsDiv.append(getSongElem(songs[i]));
+    }
+}
+
+//gets song Elem information
+function getSongElem(song){
+    let songDiv = document.createElement("div");
+    songDiv.classList.add("song");
+    let songTitle = document.createElement("h3");
+    songTitle.innerHTML = song.id + ": " + song.name;
+    
+    let songP = document.createElement("p");
+
+    songDiv.append(songTitle);
+    return songDiv;
+
+}
+
+async function getSongElemByID() {
+    const inputBoxElem = document.getElementById("song-input");
+    // construct the song path based off of inputed id
+    let songPath = 'api/songs/'+ inputBoxElem.value;
+    // fetch the song
+    let songJson = await fetch(songPath);
+    let song = await songJson.json();
+    
+    //remove old song titles
+    let oldSongElem = document.getElementById("song-title");
+    if (oldSongElem != null){
+        oldSongElem.remove();
+    }
+
+    // add result to song div
+    let songDiv = document.getElementById("songs");
+    let songTitle = document.createElement("h3");
+    songTitle.innerHTML = song.id + ": " + song.name;
+    songTitle.id = "song-title";
+    songDiv.append(songTitle);
+}
+
+
+window.onload = function(){
+    //this.showSongs();
+    const viewSongButtonElem = this.document.getElementById("view-song");
+    viewSongButtonElem.onclick = this.getSongElemByID;
+}
--- inclass/nov11node/server.js
+++ inclass/nov11node/server.js
@@ -4,9 +4,35 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+const songs = [
+    {id:1, name:"Jingle Bells", singer:"Michael Buble", genre:"jazz"},
+    {id:2, name:"All I Want for Christmas is you", singer:"Miriah Carrie", genre : "pop" },
+    {id:3, name:"Rudolph the Red Nose Reindeer", singer: "DMX", genre:"rap"},
+    {id:4, name:"White Christmas", singer:"Elvis Presley", genre:"rock"}
+]
+
+// response to our songs
+app.get('/api/songs', (req,res)=>{
+    res.send(songs);
+});
+
 //render our html page
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
+});
+
+
+//get specific song
+app.get('/api/songs/:id', (req,res)=>{
+    const requestedID = parseInt(req.params.id);
+    const song = songs.find(s =>s.id === requestedID);
+
+    if(!song) {
+        res.status(404).send(`The song with id ${requestedID} was not found`);
+        return;
+    }
+    
+    res.send(song);
 });
 
 //listen 
